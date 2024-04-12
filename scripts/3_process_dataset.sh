228c7786c2570537d89103d2f45460bffd768249
Artur Szalata <arturs6668@gmail.com>
2024-04-12 12:24:58 +0200

    update the files for dataset processing

--- scripts/3_process_dataset.sh
+++ scripts/3_process_dataset.sh
@@ -2,8 +2,8 @@
 
 set -e
 
+mkdir -p resources/neurips-2023-data/
 viash run src/dge_perturbation_prediction/process_dataset/config.vsh.yaml \
-  --de_per_plate_by_celltype resources/neurips-2023-raw/de_per_plate_by_cell_type.h5ad \
-  --de_per_plate resources/neurips-2023-raw/de_per_plate.h5ad \
+  --sc_counts resources/neurips-2023-raw/sc_counts.h5ad \
   --de_train resources/neurips-2023-data/de_train.h5ad \
   --de_test resources/neurips-2023-data/de_test.h5ad
--- src/dge_perturbation_prediction/process_dataset/script.py
+++ src/dge_perturbation_prediction/process_dataset/script.py
@@ -2,16 +2,14 @@ import anndata as ad
 
 ## VIASH START
 par = {
-    "de_per_plate_by_cell_type": "resources/neurips-2023-raw/de_per_plate_by_cell_type.h5ad",
-    "de_per_plate": "resources/neurips-2023-raw/de_per_plate.h5ad",
+    "sc_counts": "resources/neurips-2023-raw/sc_counts.h5ad",
     "de_train": "resources/neurips-2023-data/de_train.h5ad",
     "de_test": "resources/neurips-2023-data/de_test.h5ad",
 }
 ## VIASH END
 
 print(">> Load dataset", flush=True)
-de_per_plate_by_cell_type = ad.read_h5ad(par["de_per_plate_by_cell_type"])
-de_per_plate = ad.read_h5ad(par["de_per_plate"])
+sc_counts = ad.read_h5ad(par["sc_counts"])
 
 print(">> Split dataset", flush=True)
 de_train = ...
