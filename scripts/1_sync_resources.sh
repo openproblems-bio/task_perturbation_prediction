2fc45db50ab208e677177441e99b99691e3d6035
Artur Szalata <arturs6668@gmail.com>
2024-04-12 12:20:56 +0200

    create data directories if they don't exist

--- scripts/1_sync_resources.sh
+++ scripts/1_sync_resources.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws s3 sync s3://openproblems-bio/public/neurips-2023-competition/sc_counts.h5ad resources/neurips-2023-raw/sc_counts.h5ad
+mkdir -p resources/neurips-2023-raw/
+aws s3 cp s3://openproblems-bio/public/neurips-2023-competition/sc_counts.h5ad resources/neurips-2023-raw/sc_counts.h5ad
