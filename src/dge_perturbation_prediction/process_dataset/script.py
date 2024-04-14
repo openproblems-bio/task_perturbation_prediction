39e287bb4f6904323e12ee3c69de91cc704d691a
Artur Szalata <arturs6668@gmail.com>
2024-04-14 15:21:32 +0200

    add dataset preprocessing

--- src/dge_perturbation_prediction/process_dataset/script.py
+++ src/dge_perturbation_prediction/process_dataset/script.py
@@ -1,4 +1,12 @@
+import pandas as pd
+import numpy as np
+
 import anndata as ad
+
+from utils import sum_by, create_split_mapping, make_r_safe_names, _run_limma_for_cell_type, convert_de_df_to_anndata, anndata_to_dataframe
+import limma_utils
+import imp
+imp.reload(limma_utils)
 
 ## VIASH START
 par = {
@@ -12,11 +20,66 @@ par = {
 
 print(">> Load dataset", flush=True)
 sc_counts = ad.read_h5ad(par["sc_counts"])
+lincs_id_compound_mapping = pd.read_parquet(par["lincs_id_compound_mapping"])
+
+print(">> Process dataset", flush=True)
+# adapt column names and data fields
+sc_counts.obs = sc_counts.obs.reset_index().rename(columns={'index': 'obs_id'})
+sc_counts.obs['control'] = sc_counts.obs['split'].eq("control")
+sc_counts.obs = sc_counts.obs.drop(columns=["hashtag_id", "raw_cell_id", "container_format"])
+sc_counts.obs["SMILES"] = sc_counts.obs["sm_name"].map(lincs_id_compound_mapping.set_index("sm_name")["smiles"])
+sc_counts.obs["sm_lincs_id"] = sc_counts.obs["sm_name"].map(lincs_id_compound_mapping.set_index("sm_name")["sm_lincs_id"])
+sc_counts.obs['library_id'] = (sc_counts.obs['plate_name'].astype(str) + '_' + sc_counts.obs['row'].astype(str)).astype('category')
+sc_counts.obs = sc_counts.obs.set_index("obs_id")
+sc_counts.obs['plate_well_cell_type'] = sc_counts.obs['plate_name'].astype('str') \
+    + '_' + sc_counts.obs['well'].astype('str') \
+    + '_' + sc_counts.obs['cell_type'].astype('str')
+sc_counts.obs['plate_well_cell_type'] = sc_counts.obs['plate_well_cell_type'].astype('category')
+
+sc_counts.X = sc_counts.layers["counts"].copy()
+del sc_counts.layers["counts"]
+
+# compute pseudobulk
+bulk_adata = sum_by(sc_counts, 'plate_well_cell_type')
+bulk_adata.obs = bulk_adata.obs.drop(columns=['plate_well_cell_type'])
+bulk_adata.X = np.array(bulk_adata.X.todense())
+
+# remove samples with no counts
+bulk_adata = bulk_adata[bulk_adata.X.sum(axis=1) > 0].copy()
+
+mapping_split = create_split_mapping(bulk_adata.obs)
+
+factors = ['sm_name', 'donor_id', 'plate_name', 'row']
+
+# Apply the renaming function only to the entries of selected columns
+for col in factors:
+    if col in bulk_adata.obs.columns:
+        bulk_adata.obs[col] = bulk_adata.obs[col].apply(make_r_safe_names)
+
+# Run limma
+cell_types = bulk_adata.obs['cell_type'].unique()
+de_dfs = []
+
+for cell_type in cell_types:
+    cell_type_selection = bulk_adata.obs['cell_type'].eq(cell_type)
+    cell_type_bulk_adata = bulk_adata[cell_type_selection].copy()
+
+    de_df = _run_limma_for_cell_type(cell_type_bulk_adata, "resources/neurips-2023-data")
+
+    de_dfs.append(de_df)
+de_df = pd.concat(de_dfs)
+de_adata = convert_de_df_to_anndata(de_df, 0.05)
+# convert anndata back to dataframe with the format of kaggle data
+de_df = anndata_to_dataframe(de_adata, mapping_split)
 
 print(">> Split dataset", flush=True)
-de_train = ...
-de_test = ...
+de_train = de_df[de_df.split != "private_test"]
+de_test = de_df[de_df.split == "private_test"].copy()
+de_test.reset_index(drop=True, inplace=True)
+de_test.reset_index(names="id", inplace=True)
+id_map = de_test[["id", "cell_type", "sm_name"]]
 
 print(">> Write to disk", flush=True)
-de_train.write_h5ad(par["de_train"])
-de_test.write_h5ad(par["de_test"])
+de_train.to_parquet(par["de_train"])
+de_test.to_parquet(par["de_test"])
+id_map.to_csv(par["id_map"], index=False)
---
+++ src/dge_perturbation_prediction/process_dataset/utils.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+from scipy import sparse
+
+import anndata as ad
+import os, binascii, re
+
+de_pert_cols = [
+    'sm_name',
+    'sm_lincs_id',
+    'SMILES',
+    'dose_uM',
+    'timepoint_hr',
+    'cell_type',
+]
+control_compound = 'Dimethyl_Sulfoxide'
+
+def sum_by(adata: ad.AnnData, col: str) -> ad.AnnData:
+    """
+    Adapted from this forum post:
+    https://discourse.scverse.org/t/group-sum-rows-based-on-jobs-feature/371/4
+    """
+
+    # assert pd.api.types.is_categorical_dtype(adata.obs[col])
+    assert isinstance(adata.obs[col].dtypes, pd.CategoricalDtype)
+
+    # sum `.X` entries for each unique value in `col`
+    cat = adata.obs[col].values
+    indicator = sparse.coo_matrix(
+        (
+            np.broadcast_to(True, adata.n_obs),
+            (cat.codes, np.arange(adata.n_obs))
+        ),
+        shape=(len(cat.categories), adata.n_obs),
+    )
+    sum_adata = ad.AnnData(
+        indicator @ adata.X,
+        var=adata.var,
+        obs=pd.DataFrame(index=cat.categories),
+    )
+
+    # copy over `.obs` values that have a one-to-one-mapping with `.obs[col]`
+    obs_cols = list(set(adata.obs.columns) - set([col]))
+
+    one_to_one_mapped_obs_cols = []
+    nunique_in_col = adata.obs[col].nunique()
+    for other_col in obs_cols:
+        if len(adata.obs[[col, other_col]].drop_duplicates()) == nunique_in_col:
+            one_to_one_mapped_obs_cols.append(other_col)
+
+    joining_df = adata.obs[[col] + one_to_one_mapped_obs_cols].drop_duplicates().set_index(col)
+    assert (sum_adata.obs.index == sum_adata.obs.join(joining_df).index).all()
+    sum_adata.obs = sum_adata.obs.join(joining_df)
+    sum_adata.obs.index.name = col
+    sum_adata.obs = sum_adata.obs.reset_index()
+    sum_adata.obs.index = sum_adata.obs.index.astype('str')
+
+    return sum_adata
+
+
+def create_split_mapping(df):
+    # Group by 'sm_name' and 'cell_type', then select the first occurrence of 'split'
+    grouped = df.groupby(['sm_name', 'cell_type']).agg({
+        'split': 'first'  # Use 'first' to grab the split for each unique group
+    }).reset_index()
+
+    # Create a dictionary with tuple keys
+    mapping_dict = {(row['sm_name'], row['cell_type']): row['split'] for index, row in grouped.iterrows()}
+
+    return mapping_dict
+
+
+def _run_limma_for_cell_type(bulk_adata, output_dir):
+    import limma_utils
+    bulk_adata = bulk_adata.copy()
+
+    compound_name_col = de_pert_cols[0]
+
+    # limma doesn't like dashes etc. in the compound names
+    rpert_mapping = bulk_adata.obs[compound_name_col].drop_duplicates() \
+        .reset_index(drop=True).reset_index() \
+        .set_index(compound_name_col)['index'].to_dict()
+
+    bulk_adata.obs['Rpert'] = bulk_adata.obs.apply(
+        lambda row: rpert_mapping[row[compound_name_col]],
+        axis='columns',
+    ).astype('str')
+
+    compound_name_to_Rpert = bulk_adata.obs.set_index(compound_name_col)['Rpert'].to_dict()
+    ref_pert = compound_name_to_Rpert[control_compound]
+
+    random_string = binascii.b2a_hex(os.urandom(15)).decode()
+
+    limma_utils.limma_fit(
+        bulk_adata,
+        design='~0+Rpert+donor_id+plate_name+row',
+        output_path=f'{output_dir}/{random_string}_limma.rds',
+        plot_output_path=f'{output_dir}/{random_string}_voom',
+        exec_path='limma_fit.r',
+        Rscript_path="/usr/local/bin/Rscript"
+    )
+
+    pert_de_dfs = []
+
+    for pert in bulk_adata.obs['Rpert'].unique():
+        if pert == ref_pert:
+            continue
+
+        pert_de_df = limma_utils.limma_contrast(
+            fit_path=f'{output_dir}/{random_string}_limma.rds',
+            contrast='Rpert' + pert + '-Rpert' + ref_pert,
+            exec_path='limma_contrast.r',
+            Rscript_path="/usr/local/bin/Rscript"
+        )
+
+        pert_de_df['Rpert'] = pert
+
+        pert_obs = bulk_adata.obs[bulk_adata.obs['Rpert'].eq(pert)]
+        for col in de_pert_cols:
+            pert_de_df[col] = pert_obs[col].unique()[0]
+        pert_de_dfs.append(pert_de_df)
+
+    de_df = pd.concat(pert_de_dfs, axis=0)
+
+    try:
+        os.remove(f'{output_dir}/{random_string}_limma.rds')
+        os.remove(f'{output_dir}/{random_string}_voom')
+    except FileNotFoundError:
+        pass
+
+    return de_df
+
+def make_r_safe_names(name):
+    # Replace invalid characters with underscores and remove leading numbers
+    safe_name = re.sub(r'\W|^(?=\d)', '_', name)
+    return safe_name
+
+
+def convert_de_df_to_anndata(de_df, de_sig_cutoff):
+    de_df = de_df.copy()
+    zero_pval_selection = de_df['P.Value'].eq(0)
+    de_df.loc[zero_pval_selection, 'P.Value'] = np.finfo(np.float64).eps
+
+    de_df['sign_log10_pval'] = np.sign(de_df['logFC']) * -np.log10(de_df['P.Value'])
+    de_df['is_de'] = de_df['P.Value'].lt(de_sig_cutoff)
+    de_df['is_de_adj'] = de_df['adj.P.Val'].lt(de_sig_cutoff)
+
+    de_feature_dfs = {}
+    for feature in ['is_de', 'is_de_adj', 'sign_log10_pval', 'logFC', 'P.Value', 'adj.P.Val']:
+        df = de_df.reset_index().pivot_table(
+            index=['gene'],
+            columns=de_pert_cols,
+            values=[feature],
+            dropna=True,
+        )
+        de_feature_dfs[feature] = df
+
+    multiindex = de_feature_dfs['sign_log10_pval'].T.index
+    de_adata = ad.AnnData(de_feature_dfs['sign_log10_pval'].T.reset_index(drop=True), dtype=np.float64)
+    de_adata.obs.index = multiindex
+    de_adata.obs = de_adata.obs.reset_index()
+    de_adata.obs = de_adata.obs.drop(columns=['level_0'])
+    de_adata.obs.index = de_adata.obs.index.astype('string')
+
+    de_adata.layers['is_de'] = de_feature_dfs['is_de'].to_numpy().T
+    de_adata.layers['is_de_adj'] = de_feature_dfs['is_de_adj'].to_numpy().T
+    de_adata.layers['logFC'] = de_feature_dfs['logFC'].to_numpy().T
+    de_adata.layers['P.Value'] = de_feature_dfs['P.Value'].to_numpy().T
+    de_adata.layers['adj.P.Val'] = de_feature_dfs['adj.P.Val'].to_numpy().T
+
+    return de_adata
+
+
+def anndata_to_dataframe(adata, split_mapping_dict):
+    # Step 1: Extract Metadata
+    metadata = adata.obs[['cell_type', 'sm_name', 'sm_lincs_id', 'SMILES']]
+    metadata['split'] = metadata.apply(
+        lambda row: split_mapping_dict.get((row['sm_name'], row['cell_type']), 'unknown'), axis=1
+    )
+    metadata["control"] = metadata["split"].eq("control")
+
+    # Step 2: Convert the main data matrix X to a DataFrame
+    gene_expression = pd.DataFrame(adata.X, columns=adata.var_names, index=adata.obs.index)
+
+    # Step 3: Merge the metadata and the gene expression data
+    full_data = pd.concat([metadata, gene_expression], axis=1).reset_index(drop=True)
+
+    return full_data
