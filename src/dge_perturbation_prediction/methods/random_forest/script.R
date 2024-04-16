3534c3825270bb42aab123814dd9c09a83207a09
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-16 14:07:17 +0200

    fix output path

--- src/dge_perturbation_prediction/methods/random_forest/script.R
+++ src/dge_perturbation_prediction/methods/random_forest/script.R
@@ -6,7 +6,7 @@ requireNamespace("pbapply", quietly = TRUE)
 par <- list(
   de_train = "resources/neurips-2023-data/de_train.parquet",
   id_map = "resources/neurips-2023-data/id_map.csv",
-  output = "resources/neurips-2023-data/prediction.csv"
+  output = "resources/neurips-2023-data/output_rf.csv"
 )
 ## VIASH END
 
