b6b845b0159b2edbecf076f6f406e8e125d65188
Artur Szalata <arturs6668@gmail.com>
2024-04-16 06:50:01 -0400

    process train and test data separately to avoid biasing training

--- src/dge_perturbation_prediction/process_dataset/script.py
+++ src/dge_perturbation_prediction/process_dataset/script.py
@@ -59,6 +59,8 @@ bulk_adata = bulk_adata[bulk_adata.X.sum(axis=1) > 0].copy()
 
 factors = ['sm_name', 'donor_id', 'plate_name', 'row']
 
+# mapping is created to recover the original names after transforming them for limma
+mapping_sm_lincs_id_to_sm_name = bulk_adata.obs.set_index('sm_lincs_id')['sm_name'].to_dict()
 # Apply the renaming function only to the entries of selected columns
 for col in factors:
     if col in bulk_adata.obs.columns:
@@ -67,35 +69,52 @@ for col in factors:
 mapping_split = create_split_mapping(bulk_adata.obs)
 # Run limma
 cell_types = bulk_adata.obs['cell_type'].unique()
-de_dfs = []
 
 # save limma output in temporary directory
 temp_prefix = os.path.expanduser('~') + '/.tmp/limma/'
 if not os.path.exists(temp_prefix):
     os.makedirs(temp_prefix)
+
+de_dfs_train = []
+de_dfs_test = []
+# introduce train/test split so that the training output isn't biased
+train_adata = bulk_adata[bulk_adata.obs['split'] != 'private_test'].copy()
+test_cell_types = bulk_adata[bulk_adata.obs['split'] == 'private_test'].obs.cell_type.unique()
 with TemporaryDirectory(prefix=temp_prefix) as tempdirname:
     data_dir = tempdirname + '/'
     for cell_type in cell_types:
-        cell_type_selection = bulk_adata.obs['cell_type'].eq(cell_type)
-        cell_type_bulk_adata = bulk_adata[cell_type_selection].copy()
-        print(f">> Run limma for {cell_type}", flush=True)
+        # calculate DE separately for test and train data to avoid bias
+        if cell_type in test_cell_types:
+            cell_type_selection = bulk_adata.obs['cell_type'].eq(cell_type)
+            cell_type_bulk_adata = bulk_adata[cell_type_selection].copy()
+            print(f">> Run limma for test data {cell_type}", flush=True)
+            de_df = _run_limma_for_cell_type(cell_type_bulk_adata, data_dir, "Rscript", meta["resources_dir"])
+            de_dfs_test.append(de_df)
+        cell_type_selection = train_adata.obs['cell_type'].eq(cell_type)
+        cell_type_bulk_adata = train_adata[cell_type_selection].copy()
+        print(f">> Run limma for train data {cell_type}", flush=True)
         de_df = _run_limma_for_cell_type(cell_type_bulk_adata, data_dir, "Rscript", meta["resources_dir"])
-    de_dfs.append(de_df)
+        de_dfs_train.append(de_df)
 print(f">> limma runs completed", flush=True)
 
-de_df = pd.concat(de_dfs)
-de_adata = convert_de_df_to_anndata(de_df, 0.05)
+de_df_train = pd.concat(de_dfs_train)
+de_df_test = pd.concat(de_dfs_test)
+de_adata_train = convert_de_df_to_anndata(de_df_train, 0.05)
+de_adata_test = convert_de_df_to_anndata(de_df_test, 0.05)
 # convert anndata back to dataframe with the format of kaggle data
-de_df = anndata_to_dataframe(de_adata, mapping_split)
+de_df_train = anndata_to_dataframe(de_adata_train, mapping_split)
+de_df_test = anndata_to_dataframe(de_adata_test, mapping_split)
 
-print(">> Split dataset", flush=True)
-de_train = de_df[de_df.split != "private_test"]
-de_test = de_df[de_df.split == "private_test"].copy()
+# convert sm_names back to original names
+de_df_test['sm_name'] = de_df_test['sm_lincs_id'].map(mapping_sm_lincs_id_to_sm_name)
+de_df_train['sm_name'] = de_df_train['sm_lincs_id'].map(mapping_sm_lincs_id_to_sm_name)
+
+de_test = de_df_test[de_df_test.split == "private_test"].copy()
 de_test.reset_index(drop=True, inplace=True)
 de_test.reset_index(names="id", inplace=True)
 id_map = de_test[["id", "cell_type", "sm_name"]]
 
 print(">> Write to disk", flush=True)
-de_train.to_parquet(par["de_train"])
+de_df_train.to_parquet(par["de_train"])
 de_test.to_parquet(par["de_test"])
 id_map.to_csv(par["id_map"], index=False)
