8a39bed0cf77d9762e25089471517a430feef997
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-16 15:12:01 +0200

    change csv to parquet

--- scripts/5_run_method.sh
+++ scripts/5_run_method.sh
@@ -5,4 +5,4 @@ set -e
 viash run src/dge_perturbation_prediction/methods/random_forest/config.vsh.yaml -- \
   --de_train resources/neurips-2023-data/de_train.parquet \
   --id_map resources/neurips-2023-data/id_map.csv \
-  --output resources/neurips-2023-data/output_rf.csv
+  --output resources/neurips-2023-data/output_rf.parquet
