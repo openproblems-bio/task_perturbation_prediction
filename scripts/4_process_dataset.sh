ca14cbde6a8db91920cdc0e312078b6ef5b21a83
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-17 16:34:55 +0200

    remove previous component; rename folder

--- scripts/4_process_dataset.sh
+++ scripts/4_process_dataset.sh
@@ -9,32 +9,32 @@ OUT=resources/neurips-2023-data
 [[ -d "$OUT" ]] || mkdir -p "$OUT"
 
 echo "Clean single-cell counts"
-viash run src/dge_perturbation_prediction/datasets/clean_sc_counts/config.vsh.yaml -- \
+viash run src/dge_perturbation_prediction/process_dataset/clean_sc_counts/config.vsh.yaml -- \
   --input "$IN/sc_counts.h5ad" \
   --lincs_id_compound_mapping "$IN/lincs_id_compound_mapping.parquet" \
   --output "$OUT/sc_counts_cleaned.h5ad"
 
 echo "Compute pseudobulk"
-viash run src/dge_perturbation_prediction/datasets/compute_pseudobulk/config.vsh.yaml -- \
+viash run src/dge_perturbation_prediction/process_dataset/compute_pseudobulk/config.vsh.yaml -- \
   --input "$OUT/sc_counts_cleaned.h5ad" \
   --output "$OUT/pseudobulk.h5ad"
 
 echo "Run limma on training set"
-viash run src/dge_perturbation_prediction/datasets/run_limma/config.vsh.yaml -- \
+viash run src/dge_perturbation_prediction/process_dataset/run_limma/config.vsh.yaml -- \
   --input "$OUT/pseudobulk.h5ad" \
   --output "$OUT/de_train.h5ad" \
   --input_splits "train;control;public_test" \
   --output_splits "train;control;public_test"
   
 echo "Run limma on test set"
-viash run src/dge_perturbation_prediction/datasets/run_limma/config.vsh.yaml -- \
+viash run src/dge_perturbation_prediction/process_dataset/run_limma/config.vsh.yaml -- \
   --input "$OUT/pseudobulk.h5ad" \
   --output "$OUT/de_test.h5ad" \
   --input_splits "train;control;public_test;private_test" \
   --output_splits "private_test"
 
 echo "Convert h5ad to parquet"
-viash run src/dge_perturbation_prediction/datasets/convert_h5ad_to_parquet/config.vsh.yaml -- \
+viash run src/dge_perturbation_prediction/process_dataset/convert_h5ad_to_parquet/config.vsh.yaml -- \
   --input_train "$OUT/de_train.h5ad" \
   --input_test "$OUT/de_test.h5ad" \
   --output_train "$OUT/de_train.parquet" \
