19b2aadeb8ad6b4899642f125b06661d7fcb6c86
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-17 23:13:15 +0200

    remove unneeded scripts

--- scripts/6_run_rf_metric.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# run metric
-viash run src/dge_perturbation_prediction/metrics/mean_rowwise_rmse/config.vsh.yaml -- \
-  --prediction resources/neurips-2023-data/output_rf.parquet \
-  --de_test resources/neurips-2023-data/de_test.parquet \
-  --output resources/neurips-2023-data/score_rf.json
