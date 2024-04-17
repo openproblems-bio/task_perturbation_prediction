931cf38f3d59ddbc76a072c76e27fe456a88be17
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-17 22:43:38 +0200

    refactor generation script

---
+++ scripts/1_generate_resources.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+IN=resources/neurips-2023-raw
+OUT=resources/neurips-2023-data
+
+[[ ! -d $IN ]] && mkdir -p $IN
+
+if [[ ! -f "$IN/sc_counts.h5ad" ]]; then
+  echo "Downloading 'sc_counts.h5ad'"
+  aws s3 cp --no-sign-request \
+    s3://openproblems-bio/public/neurips-2023-competition/sc_counts.h5ad \
+    "$IN/sc_counts.h5ad"
+fi
+if [[ ! -f "$IN/lincs_id_compound_mapping.parquet" ]]; then
+  echo "Downloading 'lincs_id_compound_mapping.parquet'"
+  aws s3 cp --no-sign-request \
+    s3://saturn-kaggle-datasets/open-problems-single-cell-perturbations-optional/lincs_id_compound_mapping.parquet \
+    "$IN/lincs_id_compound_mapping.parquet"
+fi
+
+echo "Running 'process_dataset' workflow"
+nextflow run \
+  target/nextflow/process_dataset/workflow/main.nf \
+  -profile docker \
+  -resume \
+  --sc_counts "$IN/sc_counts.h5ad" \
+  --lincs_id_compound_mapping "$IN/lincs_id_compound_mapping.parquet" \
+  --pseudobulk "pseudo_bulk.h5ad" \
+  --de_train_h5ad "de_train.h5ad" \
+  --de_train_parquet "de_train.parquet" \
+  --de_test_h5ad "de_test.h5ad" \
+  --de_test_parquet "de_test.parquet" \
+  --id_map "id_map.csv" \
+  --publish_dir "$OUT"
+
+echo "Uploading results to S3"
+aws s3 sync --profile op2 "$OUT" "s3://openproblems-bio/public/neurips-2023-competition/workflow-resources/" --delete --dryrun
--- scripts/1_sync_resources.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p resources/neurips-2023-raw/
-aws s3 cp s3://openproblems-bio/public/neurips-2023-competition/sc_counts.h5ad --no-sign-request resources/neurips-2023-raw/sc_counts.h5ad
-aws s3 cp s3://saturn-kaggle-datasets/open-problems-single-cell-perturbations-optional/lincs_id_compound_mapping.parquet --no-sign-request resources/neurips-2023-raw/lincs_id_compound_mapping.parquet
