ac263bc72bdb34da991b6dd4cd16573d8bb0b788
Artur Szalata <arturs6668@gmail.com>
2024-04-17 08:34:23 -0400

    add simple zero baseline

---
+++ src/dge_perturbation_prediction/methods/baseline_zero/script.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import json
+
+## VIASH START
+par = {
+  "de_train": "resources/neurips-2023-data/de_train.parquet",
+  "id_map": "resources/neurips-2023-data/id_map.csv",
+  "output": "resources/neurips-2023-data/output_baseline_zero.json",
+}
+## VIASH END
+
+de_train = pd.read_parquet(par["de_train"])
+id_map = pd.read_csv(par["id_map"])
+gene_names = [col for col in de_train.columns if col not in {"cell_type", "sm_name", "sm_lincs_id", "SMILES", "split", "control", "index"}]
+# create new pandas dataframe with columns "id" from id_map and gene_names for the rest. "id" fill from id_map and the rest set to zero
+output = pd.DataFrame(0, index=id_map["id"], columns=gene_names).reset_index()
+output.to_parquet(par["output"])
