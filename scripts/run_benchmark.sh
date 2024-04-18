b4510c758063c73cf2cd2da3fe696acdd2bd03e9
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-18 15:48:02 +0200

    move tracing

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -10,13 +10,10 @@ OUT="output"
 # run benchmark
 export NXF_VER=23.04.2
 
-[ -f "$OUT/trace.txt" ] && rm "$OUT/trace.txt"
-
 nextflow run . \
   -main-script target/nextflow/workflows/run_benchmark/main.nf \
   -profile docker \
   -resume \
-  -with-trace "$OUT/trace.txt" \
   --publish_dir "$OUT" \
   --output_state "state.yaml" \
   --id neurips-2023-data \
