b60d60d1e3e6b1c7d61b33716772d1e3ac0e89dd
Artur Szalata <arturs6668@gmail.com>
2024-04-22 10:22:00 -0400

    add baselines

--- src/task/control_methods/mean_across_celltypes/script.py
+++ src/task/control_methods/mean_across_celltypes/script.py
@@ -5,13 +5,14 @@ par = {
   "de_train": "resources/neurips-2023-data/de_train.parquet",
   "de_test": "resources/neurips-2023-data/de_test.parquet",
   "id_map": "resources/neurips-2023-data/id_map.csv",
-  "output": "resources/neurips-2023-data/output_baseline_zero.parquet",
+  "output": "resources/neurips-2023-data/output_mean_celltypes.parquet",
 }
 ## VIASH END
 
 de_train = pd.read_parquet(par["de_train"])
 id_map = pd.read_csv(par["id_map"])
 gene_names = [col for col in de_train.columns if col not in {"cell_type", "sm_name", "sm_lincs_id", "SMILES", "split", "control", "index"}]
-# create new pandas dataframe with columns "id" from id_map and gene_names for the rest. "id" fill from id_map and the rest set to zero
-output = pd.DataFrame(0, index=id_map["id"], columns=gene_names).reset_index()
+mean_celltype = de_train.groupby("cell_type")[gene_names].mean()
+mean_celltype = mean_celltype.loc[id_map.sm_name]
+output = pd.DataFrame(mean_celltype.values, index=id_map["id"], columns=gene_names).reset_index()
 output.to_parquet(par["output"])
--- src/task/control_methods/mean_outcome/script.py
+++ src/task/control_methods/mean_outcome/script.py
@@ -1,17 +1,18 @@
 import pandas as pd
+import numpy as np
 
 ## VIASH START
 par = {
   "de_train": "resources/neurips-2023-data/de_train.parquet",
   "de_test": "resources/neurips-2023-data/de_test.parquet",
   "id_map": "resources/neurips-2023-data/id_map.csv",
-  "output": "resources/neurips-2023-data/output_baseline_zero.parquet",
+  "output": "resources/neurips-2023-data/output_mean.parquet",
 }
 ## VIASH END
 
 de_train = pd.read_parquet(par["de_train"])
 id_map = pd.read_csv(par["id_map"])
 gene_names = [col for col in de_train.columns if col not in {"cell_type", "sm_name", "sm_lincs_id", "SMILES", "split", "control", "index"}]
-# create new pandas dataframe with columns "id" from id_map and gene_names for the rest. "id" fill from id_map and the rest set to zero
-output = pd.DataFrame(0, index=id_map["id"], columns=gene_names).reset_index()
+mean_pred = de_train[gene_names].mean(axis=0)
+output = pd.DataFrame(np.vstack([mean_pred.values] * id_map.shape[0]), index=id_map["id"], columns=gene_names).reset_index()
 output.to_parquet(par["output"])
