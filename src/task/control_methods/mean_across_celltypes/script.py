a4cbe2a92bcebca7cbaf4e3301ec2513d30d044d
Artur Szalata <arturs6668@gmail.com>
2024-04-22 10:31:05 -0400

    fix bug in mean_across_celltypes script

--- src/task/control_methods/mean_across_celltypes/script.py
+++ src/task/control_methods/mean_across_celltypes/script.py
@@ -13,6 +13,6 @@ de_train = pd.read_parquet(par["de_train"])
 id_map = pd.read_csv(par["id_map"])
 gene_names = [col for col in de_train.columns if col not in {"cell_type", "sm_name", "sm_lincs_id", "SMILES", "split", "control", "index"}]
 mean_celltype = de_train.groupby("cell_type")[gene_names].mean()
-mean_celltype = mean_celltype.loc[id_map.sm_name]
+mean_celltype = mean_celltype.loc[id_map.cell_type]
 output = pd.DataFrame(mean_celltype.values, index=id_map["id"], columns=gene_names).reset_index()
 output.to_parquet(par["output"])
