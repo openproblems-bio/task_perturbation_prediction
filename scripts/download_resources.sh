8a458177370e73a03f680cb2a796e80cd51c8a86
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-26 02:37:48 +0200

    add sync resources component (#11)

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -3,7 +3,12 @@
 set -e
 
 echo ">> Downloading resources"
-aws s3 sync --no-sign-request \
-  "s3://openproblems-bio/public/neurips-2023-competition/workflow-resources/" \
-  "resources" \
+# aws s3 sync --no-sign-request \
+#   "s3://openproblems-bio/public/neurips-2023-competition/workflow-resources/" \
+#   "resources" \
+#   --delete
+
+viash run src/common/sync_test_resources/config.vsh.yaml -- \
+  --input "s3://openproblems-bio/public/neurips-2023-competition/workflow-resources/" \
+  --output "resources" \
   --delete
