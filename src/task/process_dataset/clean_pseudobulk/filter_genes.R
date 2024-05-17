3828c7a4c6846a6c4bb397ce2cdf30a56acfef10
Artur Szałata <arturs6668@gmail.com>
2024-05-17 14:04:21 +0200

    Update limma (#32)
    * revert filtering to per-celltype
    
    * execute limma for the whole dataset
    
    * leave out intercept
    
    * leave out intercept

--- src/task/process_dataset/clean_pseudobulk/filter_genes.R
+++ src/task/process_dataset/clean_pseudobulk/filter_genes.R
@@ -20,12 +20,27 @@ limma_trafo <- function(value) {
 # Filtering data for each cell type
 filtered_gene_lists <- list()
 
-# Prepare count data for edgeR analysis
-counts <- Matrix::t(adata$X)
+# Unique cell types for processing
+cell_types <- unique(adata$obs$cell_type)
 
-d <- DGEList(counts)
-design <- model.matrix(~ 0 + sm_cell_type + donor_id, data = adata$obs %>% mutate_all(limma_trafo))
-keep <- filterByExpr(d, design)
-filtered_genes <- rownames(d)[keep]
+for (cell_type in cell_types) {
+  # Subset data by cell type
+  cell_type_adata <- adata[adata$obs$cell_type == cell_type, ]
 
-cat(filtered_genes, sep=",")
+  # Prepare count data for edgeR analysis
+  counts <- Matrix::t(cell_type_adata$X)
+
+  d <- DGEList(counts)
+  design <- model.matrix(~ 0 + sm_name + donor_id, data = cell_type_adata$obs %>% mutate_all(limma_trafo))
+  keep <- filterByExpr(d, design)
+  genes_to_keep <- rownames(d)[keep]
+
+  # Store the filtered gene list
+  filtered_gene_lists[[cell_type]] <- genes_to_keep
+}
+
+# Calculate the intersection of genes across all cell types
+common_genes <- Reduce(intersect, filtered_gene_lists)
+
+# Output common genes to standard output for Python to read
+cat(common_genes, sep=",")
