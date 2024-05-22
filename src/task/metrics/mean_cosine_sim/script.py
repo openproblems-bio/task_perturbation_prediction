c501ebc669c00a355dd40e184b8e41589946cf04
Artur Szałata <arturs6668@gmail.com>
2024-05-23 00:40:15 +0200

    add thresholded metrics (#45)
    * add thresholded metrics
    
    * fix threshold values

--- src/task/metrics/mean_cosine_sim/script.py
+++ src/task/metrics/mean_cosine_sim/script.py
@@ -4,8 +4,8 @@ import numpy as np
 
 ## VIASH START
 par = {
-    "de_test_h5ad": "resources/neurips-2023-data/de_test.h5ad",
-    "prediction": "resources/neurips-2023-data/prediction.parquet",
+    "de_test_h5ad": "resources/neurips-2023-kaggle/de_test.h5ad",
+    "prediction": "resources/neurips-2023-kaggle/output_mean_compounds.parquet",
     "method_id": "foo",
     "output": "resources/neurips-2023-data/score.h5ad",
 }
@@ -20,30 +20,56 @@ genes = list(de_test.var_names)
 de_test_X = de_test.layers["sign_log10_pval"]
 prediction = prediction[genes]
 
+print("Clipping values", flush=True)
+threshold_05 = -np.log10(0.05)
+de_test_X_clipped_05 = np.clip(de_test_X, -threshold_05, threshold_05)
+prediction_clipped_05 = np.clip(prediction.values, -threshold_05, threshold_05)
+threshold_01 = -np.log10(0.01)
+de_test_X_clipped_01 = np.clip(de_test_X, -threshold_01, threshold_01)
+prediction_clipped_01 = np.clip(prediction.values, -threshold_01, threshold_01)
+
 print("Calculate mean cosine similarity", flush=True)
 mean_cosine_similarity = 0
+mean_cosine_similarity_clipped_05 = 0
+mean_cosine_similarity_clipped_01 = 0
 for i in range(de_test_X.shape[0]):
     y_i = de_test_X[i,]
     y_hat_i = prediction.iloc[i]
+    y_i_clipped_05 = de_test_X_clipped_05[i,]
+    y_hat_i_clipped_05 = prediction_clipped_05[i]
+    y_i_clipped_01 = de_test_X_clipped_01[i,]
+    y_hat_i_clipped_01 = prediction_clipped_01[i]
 
     dot_product = np.dot(y_i, y_hat_i)
+    dot_product_clipped_05 = np.dot(y_i_clipped_05, y_hat_i_clipped_05)
+    dot_product_clipped_01 = np.dot(y_i_clipped_01, y_hat_i_clipped_01)
 
     norm_y_i = np.linalg.norm(y_i)
+    norm_y_i_clipped_05 = np.linalg.norm(y_i_clipped_05)
+    norm_y_i_clipped_01 = np.linalg.norm(y_i_clipped_01)
     norm_y_hat_i = np.linalg.norm(y_hat_i)
+    norm_y_hat_i_clipped_05 = np.linalg.norm(y_hat_i_clipped_05)
+    norm_y_hat_i_clipped_01 = np.linalg.norm(y_hat_i_clipped_01)
 
     cosine_similarity = dot_product / (norm_y_i * norm_y_hat_i)
+    cosine_similarity_clipped_05 = dot_product_clipped_05 / (norm_y_i_clipped_05 * norm_y_hat_i_clipped_05)
+    cosine_similarity_clipped_01 = dot_product_clipped_01 / (norm_y_i_clipped_01 * norm_y_hat_i_clipped_01)
 
     mean_cosine_similarity += cosine_similarity
+    mean_cosine_similarity_clipped_05 += cosine_similarity_clipped_05
+    mean_cosine_similarity_clipped_01 += cosine_similarity_clipped_01
 
 mean_cosine_similarity /= de_test_X.shape[0]
+mean_cosine_similarity_clipped_05 /= de_test_X.shape[0]
+mean_cosine_similarity_clipped_01 /= de_test_X.shape[0]
 
 print("Create output", flush=True)
 output = ad.AnnData(
     uns={
         "dataset_id": de_test.uns["dataset_id"],
         "method_id": par["method_id"],
-        "metric_ids": ["mean_cosine_sim"],
-        "metric_values": [mean_cosine_similarity]
+        "metric_ids": ["mean_cosine_sim", "mean_cosine_sim_clipped_05", "mean_cosine_sim_clipped_01"],
+        "metric_values": [mean_cosine_similarity, mean_cosine_similarity_clipped_05, mean_cosine_similarity_clipped_01]
     }
 )
 
--- src/task/metrics/mean_rowwise_error/script.py
+++ src/task/metrics/mean_rowwise_error/script.py
@@ -5,7 +5,7 @@ import numpy as np
 ## VIASH START
 par = {
     "de_test_h5ad": "resources/neurips-2023-data/de_test.h5ad",
-    "prediction": "resources/neurips-2023-data/output_rf.parquet",
+    "prediction": "resources/neurips-2023-data/prediction.parquet",
     "method_id": "foo",
     "output": "resources/neurips-2023-data/score.h5ad",
 }
@@ -20,24 +20,52 @@ genes = list(de_test.var_names)
 de_test_X = de_test.layers["sign_log10_pval"]
 prediction = prediction[genes]
 
+print("Clipping values", flush=True)
+threshold_05 = -np.log10(0.05)
+de_test_X_clipped_05 = np.clip(de_test_X, -threshold_05, threshold_05)
+prediction_clipped_05 = np.clip(prediction.values, -threshold_05, threshold_05)
+
+threshold_01 = -np.log10(0.01)
+de_test_X_clipped_01 = np.clip(de_test_X, -threshold_01, threshold_01)
+prediction_clipped_01 = np.clip(prediction.values, -threshold_01, threshold_01)
+
 print("Calculate mean rowwise RMSE", flush=True)
 mean_rowwise_rmse = 0
+mean_rowwise_rmse_clipped_05 = 0
+mean_rowwise_rmse_clipped_01 = 0
 mean_rowwise_mae = 0
+mean_rowwise_mae_clipped_05 = 0
+mean_rowwise_mae_clipped_01 = 0
 for i in range(de_test_X.shape[0]):
     diff = de_test_X[i,] - prediction.iloc[i]
+    diff_clipped_05 = de_test_X_clipped_05[i,] - prediction_clipped_05[i]
+    diff_clipped_01 = de_test_X_clipped_01[i,] - prediction_clipped_01[i]
+
     mean_rowwise_rmse += np.sqrt((diff**2).mean())
+    mean_rowwise_rmse_clipped_05 += np.sqrt((diff_clipped_05**2).mean())
+    mean_rowwise_rmse_clipped_01 += np.sqrt((diff_clipped_01**2).mean())
     mean_rowwise_mae += np.abs(diff).mean()
+    mean_rowwise_mae_clipped_05 += np.abs(diff_clipped_05).mean()
+    mean_rowwise_mae_clipped_01 += np.abs(diff_clipped_01).mean()
 
 mean_rowwise_rmse /= de_test.shape[0]
+mean_rowwise_rmse_clipped_05 /= de_test.shape[0]
+mean_rowwise_rmse_clipped_01 /= de_test.shape[0]
 mean_rowwise_mae /= de_test.shape[0]
+mean_rowwise_mae_clipped_05 /= de_test.shape[0]
+mean_rowwise_mae_clipped_01 /= de_test.shape[0]
 
 print("Create output", flush=True)
 output = ad.AnnData(
     uns={
         "dataset_id": de_test.uns["dataset_id"],
         "method_id": par["method_id"],
-        "metric_ids": ["mean_rowwise_rmse", "mean_rowwise_mae"],
-        "metric_values": [mean_rowwise_rmse, mean_rowwise_mae]
+        "metric_ids": ["mean_rowwise_rmse", "mean_rowwise_mae",
+                          "mean_rowwise_rmse_clipped_05", "mean_rowwise_mae_clipped_05",
+                          "mean_rowwise_rmse_clipped_01", "mean_rowwise_mae_clipped_01"],
+        "metric_values": [mean_rowwise_rmse, mean_rowwise_mae,
+                          mean_rowwise_rmse_clipped_05, mean_rowwise_mae_clipped_05,
+                          mean_rowwise_rmse_clipped_01, mean_rowwise_mae_clipped_01]
     }
 )
 
