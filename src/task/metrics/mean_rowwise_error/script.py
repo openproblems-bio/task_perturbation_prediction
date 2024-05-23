9cf9c8e36b424f0bf35a562e7c1b4b24a3fa35f2
Artur Szalata <arturs6668@gmail.com>
2024-05-23 16:02:13 -0400

    update threshold limits

--- src/task/metrics/mean_rowwise_error/script.py
+++ src/task/metrics/mean_rowwise_error/script.py
@@ -21,39 +21,28 @@ de_test_X = de_test.layers["sign_log10_pval"]
 prediction = prediction[genes]
 
 print("Clipping values", flush=True)
-threshold_05 = -np.log10(0.05)
-de_test_X_clipped_05 = np.clip(de_test_X, -threshold_05, threshold_05)
-prediction_clipped_05 = np.clip(prediction.values, -threshold_05, threshold_05)
-
-threshold_01 = -np.log10(0.01)
-de_test_X_clipped_01 = np.clip(de_test_X, -threshold_01, threshold_01)
-prediction_clipped_01 = np.clip(prediction.values, -threshold_01, threshold_01)
+threshold_0001 = -np.log10(0.0001)
+de_test_X_clipped_0001 = np.clip(de_test_X, -threshold_0001, threshold_0001)
+prediction_clipped_0001 = np.clip(prediction.values, -threshold_0001, threshold_0001)
 
 print("Calculate mean rowwise RMSE", flush=True)
 mean_rowwise_rmse = 0
-mean_rowwise_rmse_clipped_05 = 0
-mean_rowwise_rmse_clipped_01 = 0
+mean_rowwise_rmse_clipped_0001 = 0
 mean_rowwise_mae = 0
-mean_rowwise_mae_clipped_05 = 0
-mean_rowwise_mae_clipped_01 = 0
+mean_rowwise_mae_clipped_0001 = 0
 for i in range(de_test_X.shape[0]):
     diff = de_test_X[i,] - prediction.iloc[i]
-    diff_clipped_05 = de_test_X_clipped_05[i,] - prediction_clipped_05[i]
-    diff_clipped_01 = de_test_X_clipped_01[i,] - prediction_clipped_01[i]
+    diff_clipped_0001 = de_test_X_clipped_0001[i,] - prediction_clipped_0001[i]
 
     mean_rowwise_rmse += np.sqrt((diff**2).mean())
-    mean_rowwise_rmse_clipped_05 += np.sqrt((diff_clipped_05**2).mean())
-    mean_rowwise_rmse_clipped_01 += np.sqrt((diff_clipped_01**2).mean())
+    mean_rowwise_rmse_clipped_0001 += np.sqrt((diff_clipped_0001 ** 2).mean())
     mean_rowwise_mae += np.abs(diff).mean()
-    mean_rowwise_mae_clipped_05 += np.abs(diff_clipped_05).mean()
-    mean_rowwise_mae_clipped_01 += np.abs(diff_clipped_01).mean()
+    mean_rowwise_mae_clipped_0001 += np.abs(diff_clipped_0001).mean()
 
 mean_rowwise_rmse /= de_test.shape[0]
-mean_rowwise_rmse_clipped_05 /= de_test.shape[0]
-mean_rowwise_rmse_clipped_01 /= de_test.shape[0]
+mean_rowwise_rmse_clipped_0001 /= de_test.shape[0]
 mean_rowwise_mae /= de_test.shape[0]
-mean_rowwise_mae_clipped_05 /= de_test.shape[0]
-mean_rowwise_mae_clipped_01 /= de_test.shape[0]
+mean_rowwise_mae_clipped_0001 /= de_test.shape[0]
 
 print("Create output", flush=True)
 output = ad.AnnData(
@@ -61,11 +50,9 @@ output = ad.AnnData(
         "dataset_id": de_test.uns["dataset_id"],
         "method_id": par["method_id"],
         "metric_ids": ["mean_rowwise_rmse", "mean_rowwise_mae",
-                          "mean_rowwise_rmse_clipped_05", "mean_rowwise_mae_clipped_05",
-                          "mean_rowwise_rmse_clipped_01", "mean_rowwise_mae_clipped_01"],
+                          "mean_rowwise_rmse_clipped_0001", "mean_rowwise_mae_clipped_0001"],
         "metric_values": [mean_rowwise_rmse, mean_rowwise_mae,
-                          mean_rowwise_rmse_clipped_05, mean_rowwise_mae_clipped_05,
-                          mean_rowwise_rmse_clipped_01, mean_rowwise_mae_clipped_01]
+                          mean_rowwise_rmse_clipped_0001, mean_rowwise_mae_clipped_0001]
     }
 )
 
