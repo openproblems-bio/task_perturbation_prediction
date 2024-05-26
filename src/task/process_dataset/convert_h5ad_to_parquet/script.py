9f846416314e8381559b69b4499d774cd4d3e5e4
Robrecht Cannoodt <rcannood@gmail.com>
2024-05-26 19:41:11 +0200

    refactor transformer_ensemble and convert_h5ad_to_parquet

--- src/task/process_dataset/convert_h5ad_to_parquet/script.py
+++ src/task/process_dataset/convert_h5ad_to_parquet/script.py
@@ -11,17 +11,9 @@ par = {
 }
 ## VIASH END
 
+sys.append(meta["resources_dir"])
 
-def anndata_to_dataframe(adata):
-  metadata = adata.obs[['cell_type', 'sm_name', 'sm_lincs_id', 'SMILES', 'split', 'control']]
-
-  sign_logfc_pval = pd.DataFrame(
-    adata.layers["sign_log10_pval"],
-    columns=adata.var_names,
-    index=adata.obs.index
-  )
-
-  return pd.concat([metadata, sign_logfc_pval], axis=1).reset_index(drop=True)
+from anndata_to_dataframe import anndata_to_dataframe
 
 print(">> Load dataset", flush=True)
 input_train = ad.read_h5ad(par["input_train"])
