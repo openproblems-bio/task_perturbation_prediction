408e292208aa68abee225a5c0b6a25fb86fa46e2
Robrecht Cannoodt <rcannood@gmail.com>
2024-06-01 10:18:20 +0200

    Implement alternate stability analysis (#58)
    * add sc_counts bootstrap method
    
    * remove stability wf
    
    * add novel stability wf
    
    * fix name
    
    * also passthrough layer argument
    
    * turn r component into python component
    
    * fix for list bug
    
    * simplify wf
    
    * fix wf

--- scripts/run_stability_tw.sh
+++ scripts/run_stability_tw.sh
@@ -4,20 +4,17 @@ RUN_ID="stability_$(date +%Y-%m-%d_%H-%M-%S)"
 publish_dir="s3://openproblems-data/resources/dge_perturbation_prediction/results/${RUN_ID}"
 
 cat > /tmp/params.yaml << HERE
-id: dge_perturbation_task
-input_states: s3://openproblems-bio/public/neurips-2023-competition/workflow-resources/neurips-2023-data/state.yaml
-output_state: "state.yaml"
+id: neurips-2023-data
+sc_counts: s3://openproblems-bio/public/neurips-2023-competition/sc_counts_reannotated_with_counts.h5ad
+layer: clipped_sign_log10_pval
 publish_dir: "$publish_dir"
-rename_keys: "de_train_h5ad:de_train_h5ad,de_test_h5ad:de_test_h5ad,id_map:id_map"
-settings: '{"stability": true, "stability_obs_fraction": 0.99, "stability_var_fraction": 0.99}'
 HERE
 
 tw launch https://github.com/openproblems-bio/task-dge-perturbation-prediction.git \
   --revision main_build \
   --pull-latest \
-  --main-script target/nextflow/workflows/run_benchmark/main.nf \
+  --main-script target/nextflow/workflows/run_stability_analysis/main.nf \
   --workspace 53907369739130 \
   --compute-env 6TeIFgV5OY4pJCk8I0bfOh \
   --params-file /tmp/params.yaml \
-  --entry-name auto \
   --config src/common/nextflow_helpers/labels_tw.config
