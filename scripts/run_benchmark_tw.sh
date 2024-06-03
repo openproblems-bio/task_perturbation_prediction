600b083d1f6eb7a0a8cefee00db4ce51c466198f
Robrecht Cannoodt <rcannood@gmail.com>
2024-06-03 05:33:12 +0200

    don't run with the original kaggle dataset by default

--- scripts/run_benchmark_tw.sh
+++ scripts/run_benchmark_tw.sh
@@ -11,11 +11,11 @@ param_list:
     de_test_h5ad: "$resources_dir/neurips-2023-data/de_test.h5ad"
     id_map: "$resources_dir/neurips-2023-data/id_map.csv"
     layer: clipped_sign_log10_pval
-  - id: neurips-2023-kaggle
-    de_train_h5ad: "$resources_dir/neurips-2023-kaggle/de_train.h5ad"
-    de_test_h5ad: "$resources_dir/neurips-2023-kaggle/de_test.h5ad"
-    id_map: "$resources_dir/neurips-2023-kaggle/id_map.csv"
-    layer: sign_log10_pval
+  # - id: neurips-2023-kaggle
+  #   de_train_h5ad: "$resources_dir/neurips-2023-kaggle/de_train.h5ad"
+  #   de_test_h5ad: "$resources_dir/neurips-2023-kaggle/de_test.h5ad"
+  #   id_map: "$resources_dir/neurips-2023-kaggle/id_map.csv"
+  #   layer: sign_log10_pval
 output_state: "state.yaml"
 publish_dir: "$publish_dir"
 HERE
